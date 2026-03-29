72e60b2f9e36f759574308939b09261cb999aa5c
XCODE <damii@IMACE207-ST23.local>
2026-03-29 08:58:30 -0500

    first commit

---
+++ proyecto_alertas/Models/reporte.swift
@@ -0,0 +1,18 @@
+//
+//  Usuario.swift
+//  proyecto_alertas
+//
+//  Created by XCODE on 29/03/26.
+//
+
+struct Reporte {
+    var id : String
+    var titulo : String
+    var descripcion : String
+    var latitud : String
+    var longitud : String
+    var tipoReporte : String
+    
+    // distrito sale de latitud y longitud...?
+    
+}
---
+++ proyecto_alertas/proyecto_alertasApp.swift
@@ -0,0 +1,17 @@
+//
+//  proyecto_alertasApp.swift
+//  proyecto_alertas
+//
+//  Created by XCODE on 29/03/26.
+//
+
+import SwiftUI
+
+@main
+struct proyecto_alertasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
